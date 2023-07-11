7d3ffd7a7c3c7652b2425bb4253f23fe69b434ec
Quanbodoi <quanbeovodoi@gmail.com>
2023-07-11 15:59:25 +0700

    [UPDATED] Update code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const btnInfo = [
   {
     classes: "",
     value: 0,
-    name: `Button`,
+    name: `Input`,
     type: "input",
     disabled: true,
   },
@@ -116,16 +116,102 @@ class Model{
     this.result = '0';
   }
   onButton(value){
-    if(value != 0 && this.result === '0'){
+    if(this.result === '0'){
       this.result = ''
     }
-    console.log('run')
     this.result += value;
     this._commit(this.result);
   }
   onOperator(value){
+    const arr = this.result.split('');
+    if(arr[arr.length-1] === '/' || arr[arr.length-1] === '+' || arr[arr.length-1] === '-' || arr[arr.length-1] === '*'){
+      console.log('đã vào đây rồi');
+      arr.splice(-1,1);
+      this.result = arr.join('');
+    }
     this.result += value;
     this._commit(this.result)
+  }
+  onResult(){
+    this.result = this.calculatorAct(this.result)
+    this._commit(this.result);
+  }
+  _reset(){
+    console.log('Reseting....')
+    this.result = '0';
+    this._commit(this.result);
+  }
+  calculatorAct(input='-2+25*2-1+10'){
+    const inputArr = input.split('');
+    const reNumber = /-?\d+/g;
+    let arrNumb = input.match(reNumber);
+    if(inputArr[0] === '+' || inputArr[0] === '-' || inputArr[0] === '*' || inputArr[0] === '/'){
+      inputArr.splice(0,1);
+      input = inputArr.join('')
+    }
+    if(inputArr[inputArr.length] === '+' || inputArr[inputArr.length] === '-' || inputArr[inputArr.length] === '*' || inputArr[inputArr.length] === '/'){
+      inputArr.splice(inputArr.length,1);
+      input = inputArr.join('')
+    }
+    const reOperator = /\D+/g;
+    let arrOperator = input.match(reOperator);
+    arrNumb = arrNumb.map(element => Number(element));
+    arrOperator = arrOperator.map(element => {
+      if(element === '-'){
+        element = '+';
+      }
+      return element;
+    })
+    //Thực hiện tính toán
+    //Nhân chia trước
+    const finded = [];
+    for (let i = 0; i < arrOperator.length; i++) {
+      if (arrOperator[i].toString() === "*" || arrOperator[i].toString() === "/") {
+        finded.push(i);
+      }
+    }
+    if (finded.length > 0) {
+      let pos = finded.shift();
+      let count = 0;
+      for (let i = 0; i < arrNumb.length; i++) {
+        if (i === pos) {
+          //   console.log("Run", i, pos);
+          if (arrOperator[i].toString() === "*") arrNumb[i] *= arrNumb[i + 1];
+          if (arrOperator[i].toString() === "/") arrNumb[i] /= arrNumb[i + 1];
+          arrNumb.splice(i + 1, 1);
+          arrOperator.splice(pos, 1);
+          count++;
+          if (finded.length > 0) {
+            pos = finded.shift() - count;
+          } else {
+            pos = null;
+          }
+          i = i - 1;
+        }
+      }
+    }
+    // console.log(arrNumb)
+    //Cộng trừ
+    const resultEnd = arrNumb.reduce((preVal, currVal) => {
+      let newVal;
+      // Thực hiện phép tính 2 số tương ứng với mỗi phép đã lưu trong mảng
+      if (arrOperator.length > 0) {
+        let optHead = arrOperator.shift();
+        if (optHead.toString() === "+") {
+          newVal = Number(preVal) + Number(currVal);
+        }
+      }
+      console.log(preVal,currVal)
+      return newVal;
+    });
+    if(!isFinite(resultEnd)){
+      this._reset()
+      alert('Ko chia được cho 0!');
+    }else{
+      arrNumb = [resultEnd];
+      return resultEnd.toString();
+    }
+    console.log(arrNumb)
   }
   bindShowResult(callback){
     this.showResultChanged = callback;
@@ -185,14 +271,22 @@ class View{
   }
   bindHandleAddNumber = (handler,arr) => {
     arr.forEach((element)=>{
-      if(document.getElementById("number-"+element.value))
+      if(document.getElementById("number-"+element.value)&&element.name != 'Input')
       document.getElementById("number-"+element.value).addEventListener('click',event =>
         handler(element.value)
       )
     })
   }
   bindHandleAddOperator = (handler,arr)=>{
-    arr.forEach()
+    arr.forEach((element)=>{
+      if(document.getElementById("operator-"+element.value)&&element.name != 'Input')
+      document.getElementById("operator-"+element.value).addEventListener('click',event =>
+        handler(element.value)
+      )
+    })
+  }
+  bindHandleEndResult = (handler)=>{
+    document.getElementById("result-=").addEventListener('click',event => handler())
   }
 }
 class Controller{
@@ -201,14 +295,92 @@ class Controller{
     this.model = model;
     this.model.bindShowResult(this.showResultChanged)
     this.view.bindHandleAddNumber(this.handleAddNumber,btnInfo);
+    this.view.bindHandleAddOperator(this.handlerAddOperator,btnInfo);
+    this.view.bindHandleEndResult(this.handleEndResult)
     this.showResultChanged(this.model.result)
   }
   handleAddNumber = (value)=>{
     this.model.onButton(value);
   }
-  showResultChanged =(value)=>{
+  handlerAddOperator = (value)=>{
+    this.model.onOperator(value);
+  }
+  handleEndResult = (value)=>{
+    this.model.onResult(value)
+  }
+  showResultChanged = (value)=>{
     this.view.showResult(value);
   }
 }
 
 const calculatorApp = new Controller(new Model(),new View());
+
+
+// function calculatorAct(input='-2+25*2-1+10'){
+//   const inputArr = input.split('');
+//   const reNumber = /-?\d+/g;
+//   let arrNumb = input.match(reNumber);
+//   if(inputArr[0] === '+' || inputArr[0] === '-' || inputArr[0] === '*' || inputArr[0] === '/'){
+//     inputArr.splice(0,1);
+//     input = inputArr.join('')
+//   }
+//   const reOperator = /\D+/g;
+//   let arrOperator = input.match(reOperator);
+//   arrNumb = arrNumb.map(element => Number(element));
+//   arrOperator = arrOperator.map(element => {
+//     if(element === '-'){
+//       element = '+';
+//     }
+//     return element;
+//   })
+//   //Thực hiện tính toán
+//   //Nhân chia trước
+//   const finded = [];
+//   for (let i = 0; i < arrOperator.length; i++) {
+//     if (arrOperator[i].toString() === "*" || arrOperator[i].toString() === "/") {
+//       finded.push(i);
+//     }
+//   }
+//   if (finded.length > 0) {
+//     let pos = finded.shift();
+//     let count = 0;
+//     for (let i = 0; i < arrNumb.length; i++) {
+//       if (i === pos) {
+//         //   console.log("Run", i, pos);
+//         if (arrOperator[i].toString() === "*") arrNumb[i] *= arrNumb[i + 1]?arrNumb[i + 1]:1;
+//         if (arrOperator[i].toString() === "/") arrNumb[i] /= arrNumb[i + 1]?arrNumb[i + 1]:1;
+//         arrNumb.splice(i + 1, 1);
+//         arrOperator.splice(pos, 1);
+//         count++;
+//         if (finded.length > 0) {
+//           pos = finded.shift() - count;
+//         } else {
+//           pos = null;
+//         }
+//         i = i - 1;
+//       }
+//     }
+//   }
+//   console.log(arrNumb)
+//   //Cộng trừ
+//   const resultEnd = arrNumb.reduce((preVal, currVal) => {
+//     let newVal;
+//     // Thực hiện phép tính 2 số tương ứng với mỗi phép đã lưu trong mảng
+//     if (arrOperator.length > 0) {
+//       let optHead = arrOperator.shift();
+//       if (optHead.toString() === "+") {
+//         newVal = Number(preVal) + Number(currVal);
+//       }
+//     }
+//     console.log(preVal,currVal)
+//     return newVal;
+//   });
+//   if(resultEnd.toString() === 'Infinity'){
+//     onClear();
+//     alert('Ko chia được cho 0!');
+//   }else{
+//     arrNumb = [resultEnd];
+//   }
+//   console.log(arrNumb)
+// }
+// calculatorAct()
